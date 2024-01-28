5d105ff98f835c5d495f0f636f3d68fc71ef24e1
AsherValentini <ashermvalentini@gmail.com>
2024-01-28 13:51:09 +0100

    added comments to the basic stepper movement firmware

--- electronic_design/firmware/UNIT_TESTS/basic_stepper_driver_test/src/main.cpp
+++ electronic_design/firmware/UNIT_TESTS/basic_stepper_driver_test/src/main.cpp
@@ -1,23 +1,26 @@
 #include <Arduino.h>
 #include <FlexyStepper.h>
 
+// The following programme assumes: 
+// You are using a TMC2209 V1.3 stepper motor driver breakout board
+// You are wiring the driver to the MCU according to the MCU pinout found in electronic/hardware/schematic sheet MCU
+// You have wired the EN pin on the stepper motor driver to the common GND node. This enables the driver in hardware. Active low IC. 
+// You have wired the MS1 and MS2 pins on the stepper motor board to common GND. Hardware set for 8x microstepping. 
 
-//
-// stp and dir pin assignments (see electronic/hardware/schematic sheet MCU)
-//
+//==================================================================================================================================
+// Settings for stp and dir pin assignments (adjust if needed)
+//==================================================================================================================================
 const int MOTOR_STEP_PIN = 32;
 const int MOTOR_DIRECTION_PIN = 33;
-
-
-//
-// create the stepper motor object
-//
+//==================================================================================================================================
+// Settings for creating the stepper motor object
+//==================================================================================================================================
 FlexyStepper stepper;
-
+//==================================================================================================================================
+// Settings for microstepping 
+//==================================================================================================================================
 int micro_stepping_configuration = 8;
-
 int revolution = 200 * micro_stepping_configuration; // nema 11 is 200 steps/rev 
-
 
 
 void setup() 
@@ -33,18 +36,6 @@ void setup()
 
 void loop() 
 {
-  //
-  // Note 1: It is assumed that you are using a stepper motor with a 
-  // 1.8 degree step angle (which is 200 steps/revolution). This is the
-  // most common type of stepper.
-  //
-  // Note 2: It is also assumed that your stepper driver board is  
-  // configured for 8x microstepping.
-  //
-  // Note 3: This example uses "relative" motions.  This means that each
-  // command will move the number of steps given, starting from it's 
-  // current position.
-  //
 
   //
   // set the speed and acceleration rates for the stepper motor
@@ -53,7 +44,7 @@ void loop()
   stepper.setAccelerationInStepsPerSecondPerSecond(100);
 
   //
-  // Rotate the motor in the forward direction one revolution (200 steps). 
+  // Rotate the motor in the forward direction one revolution (1600 steps). 
   // This function call will not return until the motion is complete.
   //
   stepper.moveRelativeInSteps(revolution);
